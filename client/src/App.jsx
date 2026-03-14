483ee7067075d3395857c1f7187987f4dbc93909
Shashwat Rai <shashwatrai369@gmail.com>
2026-03-15 01:00:12 +0530

    Hero section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="flex flex-col px-5 py-10 gap-5">
+        <h1>Get Medicines Delivered to Your Doorstep</h1>
+        <p>Search medicines or upload your prescription and order from nearby pharmacies.</p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Hero;
