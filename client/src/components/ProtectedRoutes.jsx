8df289d564929a5b0b8d0a3b5c8218aafa5c246e
Shashwat Rai <shashwatrai369@gmail.com>
2026-03-22 22:08:04 +0530

    Created Protected Routes

---
+++ client/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,14 @@
+import {Navigate, replace} from 'react-router-dom';
+
+
+const ProtectedRoutes = ({children}) => {
+    const token = localStorage.getItem('token');
+
+    if(!token){
+        return <Navigate to ='/login' replace/>
+    }
+
+    return children;
+}
+
+export default ProtectedRoutes;
